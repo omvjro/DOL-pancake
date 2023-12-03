2581e86d0a1d7f81b57883792f7076edee056f8b
omvjro <omvjro@outlook.com>
2023-12-03 11:55:50 +0800

    增加链接标号；改进元素前后处理
    增加链接标号功能
    改进处理删除元素和在元素内换行的方法

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+    // 链接标号
+    let getIndex = ifChecked => document.querySelectorAll('#dol a').forEach((a, index) => {
+      if (ifChecked) {
+        a.setAttribute('data-index', `(${index + 1}) `);
+        a.setAttribute('data-if-index', '1');
+      } else { a.setAttribute('data-if-index', '0') }
+    });
+    getIndex(true);
+    document.querySelector('#link-num').addEventListener('change', () => {
+      document.querySelector('#link-num').toggleAttribute('checked');
+      getIndex(document.querySelector('#link-num').checked);
+    });
+    document.addEventListener('click', () => {
+      getIndex(document.querySelector('#link-num').checked);
+    });
+
+    // 插入元素
     let position = '';
-    document.querySelector('div.passage').addEventListener('blur', (event) => {
-      position = window.getSelection().getRangeAt(0)
+    document.querySelector('div.passage').addEventListener('blur', () => {
+      position = window.getSelection().getRangeAt(0);
     });
     document.querySelectorAll('.colorspan').forEach(sel => {
       sel.addEventListener('change', (event) => {
@@ -12,14 +29,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else {
           position.insertNode(span);
         }
-        span.outerHTML = `<${span_text} class="${event.target.value}">请输入文本</${span_text}>⁣⁣⁣⁣　`;
+        span.outerHTML = `<${span_text} class="${event.target.value}">\u200b请输入文本</${span_text}> `;
         event.target.value = '';
       });
     });
+
+    // 更换主题
     document.querySelector('#theme').addEventListener('change', (event) => {
       document.querySelector('#body').setAttribute('data-theme', event.target.value);
     });
-    document.querySelector('#pic').addEventListener('click', (event) => {
+
+    // 预览图片
+    document.querySelector('#pic').addEventListener('click', () => {
       document.querySelector('#output').innerText = '生成图片中……';
       htmlToImage.toPng(document.querySelector("#dol")).then(function(dataUrl) {
         var img = new Image();
@@ -31,7 +52,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.error('oops, something went wrong!', error);
       });
     });
-    document.querySelector('#pic-down').addEventListener('click', (event) => {
+
+    // 下载图片
+    document.querySelector('#pic-down').addEventListener('click', () => {
       document.querySelector('#output').innerText = '生成图片中……';
       htmlToImage.toBlob(document.querySelector("#dol"))
         .then(function (blob) {
@@ -43,23 +66,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
       document.querySelector('#output').innerText = '';
     });
-    document.querySelector('#code').addEventListener('click', (event) => {
+
+    // 导出代码
+    document.querySelector('#code').addEventListener('click', () => {
       let code = document.querySelector('div.passage').innerHTML;
       code = code.split('\n').map(line => {
-        return line.includes('<a class="nextWraith">') ? line
+        line = line.includes('<a class="nextWraith">') ? line
           .replace('<a class="nextWraith">', '<span id="next" class="nextWraith"><<link [[')
-          .replace('</a>', '|]]>><</link>></span>') : line
+          .replace('</a>', '|]]>><</link>></span>') : line;
+        line = line.endsWith(' ') ? line.slice(0, -1) : line;
+        return line;
       }).join('\n');
       code = code.replaceAll('\n', '\n<br>\n')
       .replaceAll('\n\n', '\n')
-      .replaceAll('>⁣⁣⁣⁣　', '>')
-      .replaceAll('<a class=" ">', '<<link [[')
+      .replaceAll('\u200b', '')
+      .replaceAll('\n<br>\n</span>', '</span>\n<br>\n')
+      .replaceAll('\n<br>\n</a>', '</a>\n<br>\n')
+      .replaceAll(/<a.*?>/gi, '<<link [[')
       .replaceAll('</a>', '|]]>><</link>>');
       navigator.clipboard.writeText(code);
       document.querySelector('#output').innerHTML = `代码已复制到剪贴板，如未成功，请在下方手动复制。<pre contenteditable="plaintext-only"></pre>`;
       document.querySelector('#output pre').innerText = code;
     });
-    document.querySelector('#clear').addEventListener("click", (event) => {
+
+    // 清空内容
+    document.querySelector('#clear').addEventListener('click', () => {
       document.querySelector('div.passage').innerText = '';
       document.querySelector('#output').innerText = '';
     });
