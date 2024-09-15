335c6dd06cde4ddf4ab036a2bc6b045166942bb2
omvjro <omvjro@outlook.com>
2024-09-15 20:26:59 +0800

    修改不同平台部署的 base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DOL-pancake/",
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue({
       template: {
