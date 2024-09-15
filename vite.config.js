c3f8ebdad92519383848079d2fcec3bc24e7d318
omvjro <omvjro@outlook.com>
2024-09-15 20:57:39 +0800

    Update deploy configs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || '/DOL-pancake/',
   plugins: [
     vue({
       template: {
